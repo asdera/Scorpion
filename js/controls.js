6fb71d6da4ec1c836767b4d8388a23d8e04bbd09
asdera <poiuytandrew@gmail.com>
2017-11-30 23:03:09 -0500

    building for itch.io

--- js/controls.js
+++ js/controls.js
@@ -309,3 +309,10 @@ function keyDown() {
 		// }
 	}
 }
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
